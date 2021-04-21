3e91bd91a007985d164dca4f82a58f4df675890c
manzumrahman <scratchedmanzum2006>
2021-04-21 19:57:31 +0600

    Patch#30

--- app.py
+++ app.py
@@ -1,9 +1,15 @@
 from website import create_app
+from dotenv import dotenv_values
 import os
 
-database_uri = os.environ.get('DATABASE_URL')
-print(database_uri)
-app = create_app(database_uri=database_uri)
+try:
+    DATABASE_URI = dotenv_values(".env")["DATABASE_URL"]
+except:
+    DATABASE_URI = os.environ.get('DATABASE_URL')
+
+
+print(DATABASE_URI)
+app = create_app(database_uri=DATABASE_URI)
 
 if __name__ == '__main__':
     app.run(debug=True)
