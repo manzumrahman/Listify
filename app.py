684c87c30bdb75c624b7daefaa0fc19244d82872
manzumrahman <scratchedmanzum2006>
2021-04-21 19:28:13 +0600

    Hided Database URI

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from website import create_app
+from dotenv import dotenv_values
 
-app = create_app()
+database_uri = dotenv_values(".env")["DATABASE_URI"]
+app = create_app(database_uri=database_uri)
 
 if __name__ == '__main__':
     app.run(debug=True)
--- website/__init__.py
+++ website/__init__.py
@@ -4,14 +4,13 @@ from flask_login import LoginManager
 
 db = flask_sqlalchemy.SQLAlchemy()
 app = flask.Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://ldxqopantlkjxr:b14179a90551c18e5855b5b62d72a2f8e473d40cdad72598eddaa3d717d6bb7c@ec2-3-234-85-177.compute-1.amazonaws.com:5432/d63mk0o6k3ulv6'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'Secret_key'
 db.__init__(app)
 
 
-
-def create_app():
+def create_app(database_uri):
+    app.config['SQLALCHEMY_DATABASE_URI'] = database_uri
     from .views import views
     from .auth import auth
 
